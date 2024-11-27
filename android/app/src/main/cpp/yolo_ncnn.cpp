f6fb43871fa4828591f4d463a58987522c4ecb14
siqi <masiqi@gmail.com>
2024-11-28 06:30:40 +0800

    添加模型

--- android/app/src/main/cpp/yolo_ncnn.cpp
+++ android/app/src/main/cpp/yolo_ncnn.cpp
@@ -1,9 +1,13 @@
 #include <jni.h>
 #include <android/log.h>
 #include <string>
+#include <net.h>  // NCNN 头文件
 
 #define TAG "YoloNcnn"
 #define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, TAG, __VA_ARGS__)
+
+// 全局变量
+static ncnn::Net* net = nullptr;
 
 extern "C" {
     JNIEXPORT jint JNICALL
@@ -24,5 +28,43 @@ extern "C" {
         int result = number * number;
         LOGD("Test compute: %d * %d = %d", number, number, result);
         return result;
+    }
+    
+    JNIEXPORT jint JNICALL
+    Java_com_boycott_app_ml_YoloDetector_loadModel(JNIEnv* env, jobject thiz, jstring paramPath, jstring binPath) {
+        const char* param = env->GetStringUTFChars(paramPath, nullptr);
+        const char* bin = env->GetStringUTFChars(binPath, nullptr);
+        
+        if (net == nullptr) {
+            net = new ncnn::Net();
+        }
+        
+        // 加载模型
+        int ret = net->load_param(param);
+        if (ret != 0) {
+            LOGD("Failed to load param file");
+            return -1;
+        }
+        
+        ret = net->load_model(bin);
+        if (ret != 0) {
+            LOGD("Failed to load model file");
+            return -2;
+        }
+        
+        env->ReleaseStringUTFChars(paramPath, param);
+        env->ReleaseStringUTFChars(binPath, bin);
+        
+        LOGD("Model loaded successfully");
+        return 0;
+    }
+    
+    JNIEXPORT void JNICALL
+    Java_com_boycott_app_ml_YoloDetector_releaseModel(JNIEnv* env, jobject thiz) {
+        if (net != nullptr) {
+            delete net;
+            net = nullptr;
+            LOGD("Model released");
+        }
     }
 }
--- android/app/src/main/java/com/boycott/app/ml/YoloDetector.kt
+++ android/app/src/main/java/com/boycott/app/ml/YoloDetector.kt
@@ -10,6 +10,12 @@ class YoloDetector {
     // 简单的测试方法，传入一个数字并返回其平方
     external fun testCompute(number: Int): Int
 
+    // 加载模型文件
+    external fun loadModel(paramPath: String, binPath: String): Int
+
+    // 释放模型资源
+    external fun releaseModel()
+
     companion object {
         init {
             System.loadLibrary("yolo_ncnn")
